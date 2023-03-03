d14369aca55a10a72d22c976049584623a4f2415
RYAN VICTOR FERREIRA CONSTANTINO DA SILVA <20231ADSGR0251@labs.garanhuns.ifpe>
2023-03-03 17:22:02 -0300

    decomporNum complete

--- python/decomporNum.py
+++ python/decomporNum.py
@@ -5,10 +5,9 @@ quantD =0
 quantU = 0
 
 if valor<1000:
-    if valor>=100:
         quantC = int(valor/100)
-        quantD = int(valor/10)
-        quantU = 0
+        quantD = int((valor-(quantC*100))/10)
+        quantU = int(valor-(quantC*100)-(quantD*100)/10)
 print(quantC , " centenas")
 print(quantD , " dezenas")
 print(quantU , " unidades")
